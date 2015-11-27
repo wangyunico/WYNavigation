34c9488c4f504dc02e9d42dc861bd0b91662c22f
wangyunico <wangyunico@gmail.com>
2015-11-27 13:27:55 +0800

    配置工程

---
+++ WYNavigation/WYNavigation/WYNavigation.h
@@ -0,0 +1,19 @@
+//
+//  WYNavigation.h
+//  WYNavigation
+//
+//  Created by apple on 15/11/27.
+//  Copyright © 2015年 wangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WYNavigation.
+FOUNDATION_EXPORT double WYNavigationVersionNumber;
+
+//! Project version string for WYNavigation.
+FOUNDATION_EXPORT const unsigned char WYNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WYNavigation/PublicHeader.h>
+
+
---
+++ WYNavigationDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WYNavigationDemo
+//
+//  Created by apple on 15/11/27.
+//  Copyright © 2015年 Neusoft. All rights reserved.
+//
+
+import UIKit
+import WYNavigation
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
